ff144c245ceecac349067b955622bd5e238a4086
RafaŁ Łukaszewski <solarplexus6@gmail.com>
2013-04-21 23:54:25 +0200

    Lista 5 - zad 1

---
+++ OppL5/Tests/Zad1Test.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Zad1;
+using netDumbster.smtp;
+
+namespace Tests
+{
+    [TestClass]
+    public class Zad1Test
+    {
+        #region Private fields
+
+        private static SimpleSmtpServer _server;
+
+        #endregion
+        #region Public static methods
+
+        [ClassInitialize]
+        public static void InitTests(TestContext context)
+        {
+            const int PORT = 2773;
+            _server = SimpleSmtpServer.Start(PORT);
+        }
+
+        #endregion
+        #region Public methods
+
+        [TestInitialize]
+        public void Init()
+        {
+            _server.ClearReceivedEmail();
+        }
+
+        const string FROM = "wieslav.dev@gmail.com";
+        const string TO = "solarplexus6@gmail.com";
+
+        [TestMethod]
+        public void TestSendWithAttachment()
+        {
+            const string SUBJECT = "smtp facade attachment test";
+            const string BODY = "attachment test body";
+            const string ATTACHMENT_CONTENT = "test att content";
+
+            var smtpFacade = new SmtpFacade();            
+
+            using (var stream = new MemoryStream(Encoding.UTF8.GetBytes(ATTACHMENT_CONTENT)))
+            {
+                smtpFacade.Send(FROM, TO, SUBJECT, BODY, stream, MediaTypeNames.Text.Plain);
+            }
+
+            var received = _server.ReceivedEmail.Single();
+            var receivedAttachment = Convert.FromBase64String(received.MessageParts[1].BodyData);
+            Assert.AreEqual(ATTACHMENT_CONTENT, Encoding.UTF8.GetString(receivedAttachment));
+        }
+
+        [TestMethod]
+        public void TestSendWithoutAttachment()
+        {
+            const string SUBJECT = "smtp facade test";
+            const string BODY = "test body";
+
+            var smtpFacade = new SmtpFacade();            
+            smtpFacade.Send(FROM, TO, SUBJECT, BODY);
+
+            var received = _server.ReceivedEmail.Single();
+            Assert.AreEqual(FROM, received.FromAddress.ToString());
+            Assert.AreEqual(TO, received.ToAddresses.Single().ToString());
+            Assert.AreEqual(BODY, received.MessageParts.Single().BodyData);
+        }
+
+        #endregion
+    }
+}
---
+++ OppL5/Zad1/SmtpFacade.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Net.Mail;
+using System.Net.Mime;
+using System.Threading.Tasks;
+
+namespace Zad1
+{
+    public class SmtpFacade
+    {
+        private const string ATTACHMENT_NAME = "attachment";
+
+        #region Public methods
+
+        public void Send(string @from, string to, string subject, string body, Stream stream = null,
+                               string attachmentMimeType = null)
+        {
+            using (var client = new SmtpClient())
+            {
+                if (stream == null || attachmentMimeType == null)
+                {
+                    client.Send(@from, to, subject, body);
+                }
+                else
+                {
+                    if (string.IsNullOrEmpty(attachmentMimeType))
+                    {
+                        throw new ArgumentNullException();
+                    }
+
+                    //message
+                    var mailMessage = new MailMessage(@from, to, subject, body);                                        
+
+                    //attachment
+                    var attachment = new Attachment(stream, new ContentType(attachmentMimeType));
+                    var disposition = attachment.ContentDisposition;
+                    // Suggest a file name for the attachment.
+                    disposition.FileName = ATTACHMENT_NAME;
+                    // Add the attachment to the message.
+                    mailMessage.Attachments.Add(attachment);
+                    client.Send(mailMessage);
+                }
+            }
+        }
+
+        #endregion
+    }
+}
